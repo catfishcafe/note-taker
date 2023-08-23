92a85a5e06ee8bf65b1f170d11fd4343c6044d30
catfishcafe <akmhobbs@gmail.com>
2023-08-22 20:30:53 -0400

    instantiated express

--- server.js
+++ server.js
@@ -2,4 +2,11 @@ const express = require('express');
 const path = require('path');
 const api = require('./routes/index.js');
 
-const PORT = 3001
+const app = express();
+
+const PORT = 3001;
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT}`)
+);
+
